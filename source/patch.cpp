4bfba7f5f77a7c523b56e497a28f976c13f53d50
heatray <heatrayws@gmail.com>
2024-03-09 01:39:26 +0300

    Fix bomber aspect ratio

--- source/patch.cpp
+++ source/patch.cpp
@@ -1,15 +1,83 @@
 #include "stdafx.h"
 
-bool DisableLetterbox, LandmineLodFix, UnlockAllLanguages, UnlockLoyaltyItems;
 uint8_t FrameInterval;
+bool AspectRatioFix, DisableLetterbox, LandmineLodFix, UnlockAllLanguages, UnlockLoyaltyItems;
+int Width = 800;
+int Height = 600;
+float AspectRatioX = 4.0f / 3.0f;
+float AspectRatioY = 3.0f / 4.0f;
+float ApertureValue = 12.7f;
 
 bool Borderless;
+
+DWORD* WormsXApp__c_pInstance = (DWORD*)0x9795A4;
+DWORD SetFromSceneCameraCodeCaveExit = 0x6E1F83;
+
+void __declspec(naked) AspectRatioCodeCave()
+{
+    __asm {
+        mov     ecx, dword ptr ds:[0x9795A4]
+        mov     ecx, [ecx+0x28]
+        mov     Width, ecx
+        mov     ecx, dword ptr ds:[0x9795A4]
+        mov     ecx, [ecx+0x2C]
+        mov     Height, ecx
+        fild    dword ptr ds:[Height]
+        fild    dword ptr ds:[Width]
+        fdivp   st(1), st(0)
+        fld     dword ptr ds:[AspectRatioY]
+        fcomip  st(0), st(1)
+        fstp    st(0)
+        jne     Hor
+        jmp     Exit
+
+    Hor:
+        jb      Vert
+        fild    dword ptr ds:[Width]
+        fild    dword ptr ds:[Height]
+        fdivp   st(1), st(0)
+        fstp    dword ptr ds:[AspectRatioX]
+        jmp     Exit
+
+    Vert:
+        fild    dword ptr ds:[Height]
+        fild    dword ptr ds:[Width]
+        fdivp   st(1), st(0)
+        fstp    dword ptr ds:[AspectRatioY]
+
+    Exit:
+        retn    8
+    }
+}
+
+void __declspec(naked) SetFromSceneCameraCodeCave()
+{
+    // Frustum.X = 25.4/2 * Aperture.Y * W/H
+    // Frustum.Y = 25.4/2 * Aperture.X * H/W
+    __asm {
+        fld     dword ptr ds:[ApertureValue]
+        fld     dword ptr ds:[AspectRatioX]
+        fmul    dword ptr ds:[eax+0x30]
+        fmul    st(0), st(1)
+        fstp    dword ptr ss:[ebp+0xC]
+        fld     dword ptr ds:[AspectRatioY]
+        fmul    dword ptr ds:[eax+0x2C]
+        fmul    st(0), st(1)
+        fstp    dword ptr ss:[ebp-4]
+        fstp    st(0)
+        push    edi
+        mov     ecx, esi
+        mov     byte ptr ds:[esi+0x11C], 0
+        jmp     SetFromSceneCameraCodeCaveExit
+    }
+}
 
 void Init()
 {
     CIniReader iniReader("");
 
     FrameInterval = iniReader.ReadInteger("Main", "FrameInterval", 16);
+    AspectRatioFix = iniReader.ReadInteger("Main", "AspectRatioFix", 0) == 1;
     DisableLetterbox = iniReader.ReadInteger("Main", "DisableLetterbox", 0) == 1;
     LandmineLodFix = iniReader.ReadInteger("Main", "LandmineLodFix", 0) == 1;
     UnlockAllLanguages = iniReader.ReadInteger("Main", "UnlockAllLanguages", 0) == 1;
@@ -17,9 +85,18 @@ void Init()
     Borderless = iniReader.ReadInteger("Window", "Borderless", 0) == 1;
 
     // Main
+
     if (FrameInterval != 16) {
         injector::WriteMemory<BYTE>(0x4D919B, FrameInterval, true);
         injector::WriteMemory<BYTE>(0x4D919F, FrameInterval, true);
+    }
+
+    if (AspectRatioFix)
+    {
+        injector::MakeJMP(0x510FA7, AspectRatioCodeCave);
+
+        // XCamera::SetFromSceneCamera
+        injector::MakeJMP(0x6E1F59, SetFromSceneCameraCodeCave);
     }
 
     if (DisableLetterbox)
